385249c124e7a7ae808de0b5fb6a7586be6c8d28
imvignesh003 <g.vignesh1523@gmail.com>
2025-06-02 11:07:49 +0530

    sync

--- src/main/java/com/kce/ump/repository/RefreshTokenRepository.java
+++ src/main/java/com/kce/ump/repository/RefreshTokenRepository.java
@@ -3,7 +3,10 @@ package com.kce.ump.repository;
 import com.kce.ump.model.auth.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    List<RefreshToken> findByUserId(String userId);
     RefreshToken findByToken(String token);
     void deleteByToken(String token);
 }
