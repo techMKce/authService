997f163359b3b57b5645f3ccf025169e7e88e266
Akash-N-D <717821l302@kce.ac.in>
2025-06-04 21:07:38 +0530

    Async used for reduce time

---
+++ src/main/java/com/kce/ump/config/ProfileApplication.java
@@ -0,0 +1,13 @@
+package com.kce.ump.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class ProfileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileApplication.class, args);
+    }
+}
